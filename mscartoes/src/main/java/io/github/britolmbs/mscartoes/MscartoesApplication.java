8e45c74c7ddd9a36d62e9c8a18ee58d913051961
britolmbs <britolmbs@hotmail.com>
2023-02-03 10:17:09 -0300

    Criação de microservice de Cartões

---
+++ mscartoes/src/main/java/io/github/britolmbs/mscartoes/MscartoesApplication.java
@@ -0,0 +1,13 @@
+package io.github.britolmbs.mscartoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscartoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscartoesApplication.class, args);
+	}
+
+}
---
+++ mscartoes/src/test/java/io/github/britolmbs/mscartoes/MscartoesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.britolmbs.mscartoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscartoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
