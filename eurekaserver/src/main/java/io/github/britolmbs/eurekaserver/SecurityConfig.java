6ff165510199782ce93c26a7720b533a70134101
britolmbs <britolmbs@hotmail.com>
2023-06-14 11:42:55 -0300

    Protegendo o Discovery Server com Spring Security

---
+++ eurekaserver/src/main/java/io/github/britolmbs/eurekaserver/SecurityConfig.java
@@ -0,0 +1,21 @@
+package io.github.britolmbs.eurekaserver;
+
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurer {
+
+    @Override
+    protected void configure (HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+}
